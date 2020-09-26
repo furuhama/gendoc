549179e4ecc1dcd6f97a1cfa28b644f7d4f08084
furuhama <furuhama@users.noreply.github.com>
2020-09-26 20:49:44 +0900

    Refactor to create DocumentOption struct

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,25 @@
 use std::fs::{File, OpenOptions};
 use std::io::prelude::*;
 
+struct DocumentOption {
+    filename: String,
+    body: String,
+}
+
+impl DocumentOption {
+    fn convert(&mut self) {
+        let conv = |s: &str| {
+            s.replace(
+                "<date>",
+                &chrono::Local::today().format("%Y%m%d").to_string(),
+            )
+        };
+
+        conv(&mut self.filename);
+        conv(&mut self.body);
+    }
+}
+
 fn main() {
     let mut template_file = File::open("./gendoc.yaml").expect("gendoc.yaml is not found");
     let mut contents = String::new();
@@ -13,23 +32,27 @@ fn main() {
     let filename = template_map.get("filename").unwrap();
     let body = template_map.get("body").unwrap();
 
-    let filename = convert(filename);
-    let body = convert(body);
+    let mut document_option = DocumentOption {
+        filename: filename.to_owned(),
+        body: body.to_owned(),
+    };
 
-    let mut file = OpenOptions::new()
-        .write(true)
-        .create_new(true)
-        .open(&filename)
-        .expect(&format!("target filename {} already exists", filename));
+    document_option.convert();
 
-    file.write_all(body.as_bytes()).unwrap();
+    generate(&document_option);
 
     println!("Document generated: {}", filename);
 }
 
-fn convert(s: &str) -> String {
-    s.replace(
-        "<date>",
-        &chrono::Local::today().format("%Y%m%d").to_string(),
-    )
+fn generate(document_option: &DocumentOption) {
+    let mut file = OpenOptions::new()
+        .write(true)
+        .create_new(true)
+        .open(&document_option.filename)
+        .expect(&format!(
+            "target filename {} already exists",
+            document_option.filename
+        ));
+
+    file.write_all(document_option.body.as_bytes()).unwrap();
 }
