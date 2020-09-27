7d1b907455287598cf41f340baa93ae3edc97c12
furuhama <furuhama@users.noreply.github.com>
2020-09-27 23:48:33 +0900

    Rename method & Refactor test

--- src/option.rs
+++ src/option.rs
@@ -10,20 +10,20 @@ impl Option {
     }
 
     pub fn convert(&mut self) {
-        self.filename = Self::_convert_field(&self.filename);
-        self.body = Self::_convert_field(&self.body);
-        self.dir = Self::_convert_field(&self.dir);
+        self.filename = Self::convert_field(&self.filename);
+        self.body = Self::convert_field(&self.body);
+        self.dir = Self::convert_field(&self.dir);
     }
 
-    fn _convert_field(s: &str) -> String {
-        let result = Self::_convert_meta_tag(
+    fn convert_field(s: &str) -> String {
+        let result = Self::convert_meta_tag(
             "date",
             |s: &str| chrono::Local::today().format(s).to_string(),
             "%Y%m%d",
             s,
         );
 
-        let result = Self::_convert_meta_tag(
+        let result = Self::convert_meta_tag(
             "datetime",
             |s: &str| chrono::Local::now().format(s).to_string(),
             "%Y%m%d%H%M%S",
@@ -33,7 +33,7 @@ impl Option {
         result
     }
 
-    fn _convert_meta_tag<F>(tag_name: &str, formatter: F, default_arg: &str, s: &str) -> String
+    fn convert_meta_tag<F>(tag_name: &str, formatter: F, default_arg: &str, s: &str) -> String
     where
         F: Fn(&str) -> String,
     {
@@ -63,33 +63,36 @@ mod tests {
 
     #[test]
     fn test_document_option_convert_field() {
-        assert_eq!(
-            Option::_convert_field("without meta tag"),
-            "without meta tag"
-        );
-        assert_eq!(
-            Option::_convert_field("<date>"),
-            chrono::Local::today().format("%Y%m%d").to_string()
-        );
-        assert_eq!(
-            Option::_convert_field("<date:%Y-%m-%d>"),
-            chrono::Local::today().format("%Y-%m-%d").to_string()
-        );
-        assert_eq!(
-            Option::_convert_field("<datetime>"),
-            chrono::Local::now().format("%Y%m%d%H%M%S").to_string()
-        );
-        assert_eq!(
-            Option::_convert_field("<datetime:%Y-%m-%d-%H%M%S>"),
-            chrono::Local::now().format("%Y-%m-%d-%H%M%S").to_string()
-        );
-        assert_eq!(
-            Option::_convert_field("<date:%Y-%m-%d><datetime:%Y-%m-%d-%H%M%S>"),
-            format!(
-                "{}{}",
-                chrono::Local::today().format("%Y-%m-%d").to_string(),
-                chrono::Local::now().format("%Y-%m-%d-%H%M%S").to_string()
-            )
-        );
+        let pair = [
+            ("without meta tag", "without meta tag"),
+            (
+                "<date>",
+                &chrono::Local::today().format("%Y%m%d").to_string(),
+            ),
+            (
+                "<date:%Y-%m-%d>",
+                &chrono::Local::today().format("%Y-%m-%d").to_string(),
+            ),
+            (
+                "<datetime>",
+                &chrono::Local::now().format("%Y%m%d%H%M%S").to_string(),
+            ),
+            (
+                "<datetime:%Y-%m-%d-%H%M%S>",
+                &chrono::Local::now().format("%Y-%m-%d-%H%M%S").to_string(),
+            ),
+            (
+                "<date:%Y-%m-%d><datetime:%Y-%m-%d-%H%M%S>",
+                &format!(
+                    "{}{}",
+                    chrono::Local::today().format("%Y-%m-%d").to_string(),
+                    chrono::Local::now().format("%Y-%m-%d-%H%M%S").to_string()
+                ),
+            ),
+        ];
+
+        for (actual, expect) in pair.iter() {
+            assert_eq!(Option::convert_field(*actual), *expect);
+        }
     }
 }
